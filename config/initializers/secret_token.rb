e234486c259e21d758acd12b4be12a1ced977579
ank13 <avi.kaufman@gmail.com>
2013-08-17 13:09:35 -0400

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackplay::Application.config.secret_token = '9d48c77064193914f622e00b9e078725d23ed645a20d3c41625c9b0d71753a2edeaf8c947f7263ea92e7d6afb150beb43cd5448f7ee54bff5e01b8251afdea45'
